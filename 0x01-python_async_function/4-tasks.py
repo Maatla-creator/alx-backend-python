b9e516d8ab65e332b1645fa8f639cbbee8881cf3
Maatla-creator <maatlamog@gmail.com>
2022-04-23 21:04:52 +0000

    fix file

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,6 +3,7 @@
 This script contains an async routine called wait_n
 that takes in 2 int arguments (in this order): n and max_delay.
 It spawn wait_random n times with the specified max_delay.
+
 wait_n returns the list of all the delays (float values).
 The list of the delays should be in ascending order without using sort()
 because of concurrency.
